1366eec2b4a3ea655ee64ab66418ebf95385bbdc
panyonglong <1542330189@qq.com>
2018-02-26 16:30:17 +0800

    调用cnode社区接口

--- src/main.js
+++ src/main.js
@@ -2,12 +2,16 @@
 import Vue from 'vue'
 // 引用路由
 import VueRouter from 'vue-router'
+// 光引用不成 还要使用
+Vue.use(VueRouter)
 // 入口文件为src/App.vue 文件 
 import App from './App.vue'
 // 引用路由配置文件
 import routes from './config/routes'
-// 光引用不成 还要使用
-Vue.use(VueRouter)
+// 引入api文件
+import api from './config/api'
+// 将api方法绑定到全局
+Vue.prototype.$api = api
 // 使用配置文件规则
 const router = new VueRouter({
 	routes
