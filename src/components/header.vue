1f8432ba6cb686a9dedc6505eb36866243c3ad5d
panyonglong <1542330189@qq.com>
2018-04-09 16:45:32 +0800

    开始修改header

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,5 +1,37 @@
 <template>
 	<header class='header'>
-		<h1 class='logo'>Vue Demo by Fungleo</h1>
+		<div class='logo'>
+			<img src='/static/image/dota.jpg' alt='logo'>
+		</div>
+		<div class='menu'>
+			<dl>房间</dl>
+		</div>
+
+		<div class='userinfo'>
+			
+		</div>
 	</header>
 </template>
+
+<script>
+export default{
+	name:'header',
+	data(){
+		return {
+		}
+	},
+	props:{
+		user:{}, 
+	},
+	created(){
+	
+	},
+	methods:{
+	},
+}
+<script>
+
+<style scoped>
+.header{
+}
+</style>
