5a53fac7a1bc05a777dc8c1929d41fdaddb7d8b8
panyonglong <1542330189@qq.com>
2018-04-09 14:32:29 +0800

    创建开发分支dev

--- src/components/chat.vue
+++ src/components/chat.vue
@@ -1,10 +1,12 @@
 <template>
 	<div>
-		<div id='chat'></div>
+		<div class='roominfo'></div>
+		<div id='msglog'></div>
+		<textarea id='msg'></textarea>
+		<button type='button' @click='sendMsg'>发送</button>
 	</div>
 </template>
 
-<script src='static/js/test.js'></script>
 <script>
 import { getJS } from '@/api/utils.js'
 
@@ -12,6 +14,7 @@ export default{
 	name:'chat',
 	data(){
 		return {
+			socket:null,
 		}
 	},
 	props:{
@@ -23,14 +26,48 @@ export default{
 			'https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.3/socket.io.js',
 		]
 		getJS(js_list).then(() => {
-			let socket = io('http://192.168.0.60:8000');
-			socket.on('news', data => {
-				console.log('data');
-				socket.emit('my other event', { my:'data', name:_this.name} );
-			});
+			_this.start();
 		}).catch(e => console.log(e))
 	},
 	methods:{
+		start(){
+			let socket = io('http://192.168.0.60:8000');
+			this.socket = socket;
+			let roomid = 'test';
+			socket.on('open', data => {
+				console.log('ws open');
+				socket.emit('join', { name:this.name, roomid:roomid });
+			});
+
+			socket.on('join', data => {
+				//其他人加入
+				console.log('join');
+				console.log(data);
+			});
+
+			socket.on('roominfo', data => {
+				//进入房间并获得房间信息
+				console.log('roominfo');
+				console.log(data);
+			});
+
+			socket.on('leave', data => {
+				//其他人离开房间
+				console.log('leave');
+				console.log(data);
+			});
+
+			socket.on('msg', data => {
+				//收到信息
+				console.log('msg');
+				console.log(data);
+			});
+			
+		},
+		sendMsg(){
+			let msg = 'send msg test';
+			this.socket.send(msg);
+		},
 	},
 	destroyed(){
 	}
